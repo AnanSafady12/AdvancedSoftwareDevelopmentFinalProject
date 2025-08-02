65a7c89780ea91ef7ad83d13f38d9375e0a8ba5e
AnanSafady12 <anansafady73@gmail.com>
2025-08-03 01:28:05 +0300

    Switch to Railway environment variable for Firebase Service Account

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,15 +1,6 @@
 import admin from 'firebase-admin';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import fs from 'fs';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const serviceAccount = JSON.parse(
-    fs.readFileSync(path.join(__dirname, '../serviceAccountKey.json'), 'utf8')
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
